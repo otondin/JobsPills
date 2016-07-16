f73a767582c0a07314baee6b981bfa57552e9485
Gabriel T <tondingabriel@gmail.com>
2016-07-16 16:58:35 -0300

    Adding Cocoapods to control project dependencies, SwiftyJSON framework and sentences JSON file.

--- JobsPills/ViewController.swift
+++ JobsPills/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
-
+    
+    var sentences: JSON = JSON.null
+    
+    // Interface components
+    @IBOutlet weak var lblChapter: UILabel!
+    @IBOutlet weak var lblSentence: UILabel!
+    @IBOutlet weak var lblDivider: UILabel!
+    @IBOutlet weak var lblInfoYear: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            randomizeSentence()
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        randomizeSentence()
+    }
+    
+    // Randomize a sentence to show
+    func randomizeSentence() {
+        let position = Int(arc4random_uniform(UInt32(sentences.count)))
+        
+        let chapter: String? = String(sentences[position]["Chapter"])
+        let sentence: String? = String(sentences[position]["Text"])
+        let info: String? = String(sentences[position]["Info"])
+        let media: String? = String(sentences[position]["Media"])
+        let year: String? = String(sentences[position]["Year"])
+        
+        lblChapter.text = chapter
+        lblChapter.hidden = false
+        
+        lblSentence.text = sentence
+        
+        lblDivider.text = "___"
+        lblDivider.hidden = false
+        
+        if info != nil {
+            lblInfoYear.text = info
+            lblInfoYear.hidden = false
+            if media != nil {
+                lblInfoYear.text = info! + ", " + media!
+                lblInfoYear.hidden = false
+                if year != nil {
+                    lblInfoYear.text = info! + ", " + media! + ", " +  year!
+                    lblInfoYear.highlighted = false
+                }
+            }
+        } else {
+            if media != nil {
+                lblInfoYear.text = media
+                lblInfoYear.hidden = false
+                if year != nil {
+                    lblInfoYear.text = media! + ", " + year!
+                    lblInfoYear.highlighted = false
+                }
+            }
+        }
+        
+    }
+    
 }
-
---
+++ Pods/Target Support Files/Pods-JobsPills/Pods-JobsPills-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JobsPills : NSObject
+@end
+@implementation PodsDummy_Pods_JobsPills
+@end
